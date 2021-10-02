8f92a95b3a0152379cde920a5b79f99ee576c9e4
enzobrigati <pro.enzobrg@gmail.com>
2021-10-02 16:40:44 +0200

    update dependencies and migrations

--- migrations/Version20211002142053.php
+++ migrations/Version20211002142053.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210910211831 extends AbstractMigration
+final class Version20211002142053 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20210910211831 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE paste ADD user_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE paste ADD CONSTRAINT FK_9C567898A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_9C567898A76ED395 ON paste (user_id)');
+        $this->addSql('ALTER TABLE paste ADD uuid VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE paste DROP FOREIGN KEY FK_9C567898A76ED395');
-        $this->addSql('DROP INDEX IDX_9C567898A76ED395 ON paste');
-        $this->addSql('ALTER TABLE paste DROP user_id');
+        $this->addSql('ALTER TABLE paste DROP uuid');
     }
 }
--- migrations/Version20211002142114.php
+++ migrations/Version20211002142114.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210925151848 extends AbstractMigration
+final class Version20211002142114 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20210925151848 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE report ADD created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', ADD reason LONGTEXT NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_9C567898D17F50A6 ON paste (uuid)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE report DROP created_at, DROP reason');
+        $this->addSql('DROP INDEX UNIQ_9C567898D17F50A6 ON paste');
     }
 }
